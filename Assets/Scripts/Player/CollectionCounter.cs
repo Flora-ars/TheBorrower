a7d4f8e1913c0cb2e7c8a5beb569810aa55d16f8
Flora-ars <isabelmborrero@gmail.com>
2023-09-14 17:38:23 -0500

    Changes in collecting items and canvas ui
    1. collecting script
    2. berries prefab changes collider rigid body
    3. adding rigid body

--- Assets/Scripts/Player/CollectionCounter.cs
+++ Assets/Scripts/Player/CollectionCounter.cs
@@ -1,21 +1,48 @@
+using TMPro;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectionCounter : MonoBehaviour
 {
-    private int counter = 0;
+    private int counterStrawberry = 6;
+    private int counterMushroom = 7;
+    private int counterAcorn = 6;
+    [SerializeField] private TextMeshProUGUI StrawberryCounterText;
+    [SerializeField] private TextMeshProUGUI MushroomCounterText;
+    [SerializeField] private TextMeshProUGUI AcornCounterText;
 
-    private void OnTriggerEnter(Collider other)
+    void Start() //funciona
     {
-        if (other.gameObject.CompareTag(Collector.itemCollect))
+        StrawberryCounterText.text = " " + counterStrawberry; 
+        MushroomCounterText.text = " " + counterMushroom;
+        AcornCounterText.text = " " + counterMushroom;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Strawberry"))
         {
             Destroy(other.gameObject);
-            counter++;
-
-            Debug.Log(counter);
+            counterStrawberry--;
+            StrawberryCounterText.text = " " + counterStrawberry;
+            Debug.Log("counterStrawberry");
         }
+        if (other.CompareTag("Mushroom"))
+        {
+            Destroy(other.gameObject);
+            counterMushroom--;
+            MushroomCounterText.text = " " + counterMushroom;
+            Debug.Log("counterMushroom");
+        }
+        if (other.CompareTag("Acorn")) 
+        {
+            Destroy(other.gameObject);
+            counterAcorn--;
+            AcornCounterText.text = " " + counterAcorn;
+            Debug.Log("counterAcorn");
+        }
+
     }
-        
-    
+
 }
