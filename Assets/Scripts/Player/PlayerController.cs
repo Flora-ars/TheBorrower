e06df99bf59667d18c4a40f1a1209eadede7a18f
sebasrvallejo <sebasx23@hotmail.es>
2023-09-14 11:04:44 -0500

    Scale enviroment and character. Fix PlayerController Script

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Transform playerTransform;
     Transform objectiveToScale;
     [SerializeField] float gradesMultiply;
     float movX, movZ;
-    [SerializeField] float speed;
-    [SerializeField] Rigidbody myRb;
+    
     Vector3 moveInput;
     Quaternion rotateInput;
+    
+    [SerializeField] float movementSpeed;
+    private float turnSpeed = 200;
+    private Rigidbody myRb;
+   
 
     void Start()
     {
@@ -20,29 +26,37 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         movX = Input.GetAxis("Horizontal");
         movZ = Input.GetAxis("Vertical");
 
-    }
+    }*/
     private void FixedUpdate()
     {
-
-        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
-        {
-            ControlMovement();
-        }
+       ControlMovement();
 
     }
     void ControlMovement()
     {
-        moveInput = new Vector3(movX, moveInput.y, movZ);
+        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 moveDirection = transform.forward * (verticalInput * movementSpeed);
+        myRb.velocity = moveDirection;
+        
+        this.transform.Rotate(turnSpeed * Time.deltaTime * Vector3.up * horizontalInput);
+
+        
+        
+        /*moveInput = new Vector3(movX, moveInput.y, movZ);
         Vector3 directionToMove = myRb.rotation * moveInput;
         rotateInput = Quaternion.Euler(rotateInput.x, movX * gradesMultiply, rotateInput.z);
 
         myRb.MovePosition(myRb.position + directionToMove * speed * Time.fixedDeltaTime);
-        myRb.MoveRotation(myRb.rotation * rotateInput);
+        //myRb.MoveRotation(myRb.rotation * rotateInput);
+        transform.Rotate(turnSpeed * Time.deltaTime * Vector3.up *  movZ);*/
 
     }
 }
